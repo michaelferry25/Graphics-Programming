75917c48a8c72467b2f5fc5147addee4769385c5
michaelf25 <g00420185@atu.ie>
2024-11-18 16:53:34 +0000

    Added 13x13 blur to image

--- Lab 8/lab8.py
+++ Lab 8/lab8.py
@@ -12,7 +12,8 @@ ncols = 2
 blurredGray = cv2.GaussianBlur(gray_image,(7,7),0)
 
 
-plt.subplot(nrows, ncols,1),plt.imshow(img, cmap = 'gray')
+plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(img, 
+cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 
 plt.subplot(nrows, ncols,2),plt.imshow(gray_image, cmap = 'gray')
@@ -21,7 +22,7 @@ plt.title("GRAY SCALE"), plt.xticks([]), plt.yticks([])
 plt.subplot(nrows, ncols,3),plt.imshow(blurredGray, cmap = 'gray')
 plt.title('Original Blur'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(nrows, ncols,4),plt.imshow(gray_image, cmap = 'gray')
-plt.title("GRAY SCALE BLUR"), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,4),plt.imshow(cv2.GaussianBlur(gray_image,(13,13),4), cmap = 'gray')
+plt.title("GRAY SCALE BLUR 13x13"), plt.xticks([]), plt.yticks([])
 
 plt.show()
