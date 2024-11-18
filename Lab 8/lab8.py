2f76059ec8365d7dafdaea77756683ea2a648de4
michaelf25 <g00420185@atu.ie>
2024-11-18 16:27:26 +0000

    Columns & rows to display the original and grayImg

--- Lab 8/lab8.py
+++ Lab 8/lab8.py
@@ -6,3 +6,13 @@ img = cv2.imread("ATU.jpg")
 
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imwrite('./ATU_GRAY.jpg',gray_image)
+
+nrows = 2
+ncols = 2
+
+plt.subplot(nrows, ncols,1),plt.imshow(img, cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(nrows, ncols,2),plt.imshow(gray_image, cmap = 'gray')
+plt.title("GRAY SCALE"), plt.xticks([]), plt.yticks([])
+plt.show()
