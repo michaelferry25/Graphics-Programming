db775d264dead0a857cd5fecb5b775b841576d52
michaelferry25 <g00420185@atu.ie>
2024-12-15 22:21:10 +0000

    Added Shi tomasi and ORB image in the display

--- Lab 9/lab9.py
+++ Lab 9/lab9.py
@@ -2,36 +2,53 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Reads the image
 img = cv2.imread("ATU1.jpg")
 
-rows = 3
-cols = 3
 
-# Converts the image to grayscale
+rows = 3 # How much rows in our popup
+cols = 3 # How much columns in our popup
+
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-imgHarris = gray_image.copy()
-
+imgHarris = img.copy()
 blockSize = 2
 aperture_size = 3
 k = 0.04
-
-dst = cv2.cornerHarris(imgHarris, blockSize, aperture_size, k)
-
+dst = cv2.cornerHarris(gray_image, blockSize, aperture_size, k)
 threshold = 0.04; 
 B = 0
 G = 0
-R = 0
+R = 255
+
 for i in range(len(dst)):
     for j in range(len(dst[i])):
         if dst[i][j] > (threshold*dst.max()):
             cv2.circle(imgHarris,(j,i),3,(B, G, R),-1)
 
-# Checks if image is loaded correctly
-if img is None:
-    print("Error: Could not load image.")
-    exit()
+imgShiTomasi = img.copy()
+
+maxCorners = 100
+qualityLevel = 0.01
+minDistance = 10
+corners = cv2.goodFeaturesToTrack(gray_image,maxCorners,qualityLevel,minDistance)
+corners = np.int8(corners)
+
+for i in corners:
+    x,y = i.ravel()
+    cv2.circle(imgShiTomasi,(x,y),3,(B, G, R),-1)
+
+
+orbImage = cv2.imread('ATU1.jpg', cv2.IMREAD_GRAYSCALE)
+ 
+# Initiate ORB detector
+orb = cv2.ORB_create()
+ 
+# find the keypoints with ORB
+kp = orb.detect(orbImage,None)
+ 
+# compute the descriptors with ORB
+kp, des = orb.compute(orbImage, kp)
+
 
 # Original Image
 plt.subplot(cols, cols,1)
@@ -55,11 +72,18 @@ plt.title("Harris Corners")
 plt.xticks([])
 plt.yticks([])
 
+#Shi Tomasi
 plt.subplot(cols, cols,4)
-plt.imshow(cv2.cvtColor(imgHarris, cv2.COLOR_BGR2RGB))
-plt.title("Harris Corners")
+plt.imshow(cv2.cvtColor(imgShiTomasi, cv2.COLOR_BGR2RGB))
+plt.title("Shi Tomasi")
 plt.xticks([])
 plt.yticks([])
 
-plt.tight_layout()
+# Orb Image
+plt.subplot(cols, cols,5)
+plt.imshow(cv2.drawKeypoints(orbImage, kp, None, color=(0,255,0), flags=0))
+plt.title("ORB Image")
+plt.xticks([])
+plt.yticks([])
+
 plt.show()
