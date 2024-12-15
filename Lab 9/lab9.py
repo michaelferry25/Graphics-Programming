749a44c3f688d3045a43154f284c91889c6362fc
michaelferry25 <g00420185@atu.ie>
2024-12-15 21:50:47 +0000

    Added dst and added the plot corners

--- Lab 9/lab9.py
+++ Lab 9/lab9.py
@@ -2,18 +2,37 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Read the image
+# Reads the image
 img = cv2.imread("ATU1.jpg")
 
-# Check if image is loaded correctly
+# Converts the image to grayscale
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+imgHarris = gray_image.copy()
+
+blockSize = 2
+aperture_size = 3
+k = 0.04
+
+dst = cv2.cornerHarris(imgHarris, blockSize, aperture_size, k)
+
+threshold = 0.04; 
+B = 0
+G = 0
+R = 0
+for i in range(len(dst)):
+    for j in range(len(dst[i])):
+        if dst[i][j] > (threshold*dst.max()):
+            cv2.circle(imgHarris,(j,i),3,(B, G, R),-1)
+            
+# Checks if image is loaded correctly
 if img is None:
     print("Error: Could not load image.")
     exit()
 
-# Convert the image to grayscale
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Display the original and grayscale images using matplotlib
+
+# Displays the original and grayscale images using matplotlib
 plt.figure(figsize=(10, 5))
 
 # Original image (BGR converted to RGB for Matplotlib)
